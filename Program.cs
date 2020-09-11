4b42889e3dc66ec0dd05af48ae8e5003df1e461d
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2020-09-11 10:06:54 +0430

    seperate functions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Flurl.Http;
 using HtmlAgilityPack;
@@ -6,25 +7,99 @@ namespace gal_dl
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
 
             Console.Title = "Gallery Downloader";
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            // Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.Clear();
+            
+            bool ValidInput = false;
+            int input = 0;
 
-            string URL = "https://www.pornpics.com/galleries/puba-network-anna-bell-peaks-jenevieve-hexxx-74787898/";
+            while(!ValidInput)
+            {
+                Console.WriteLine("[1] Single Download");
+                Console.WriteLine("[2] Multi Download");
+                Console.WriteLine("[3] Exit!");
+                Console.Write("==> ");
+                int temp = int.Parse(Console.ReadLine());
+                if(temp > 3)
+                {
+                    Console.WriteLine("[-] Invalid Input!");
+                }
+                else
+                {
+                    input = temp;
+                    ValidInput = true;
+                }
+            }
+
+            GC.Collect();
+
+            switch (input)
+            {
+                case 1:
+                    SingleDownloadAsync();
+                break;
+
+                case 2:
+                    MultiDownloadAsync();
+                break;
+
+                case 3:
+                break;
+
+                default:
+                break;
+            }
+
+
+        }
+
+        public static async void SingleDownloadAsync()
+        {
+            Console.Clear();
+            Console.WriteLine("[+] Single Download Mode");
+            Console.Write("Type in a URL to get started => ");
+            string URL = Console.ReadLine();
+            string folderName = URL.Remove(0, 35).Replace("/", "");
             HtmlWeb web = new HtmlWeb();
-            FlurlRequest f = new FlurlRequest(URL);
             var htmlDoc = web.Load(URL);
-
+            string path = $@"C:\\Users\\{Environment.UserName}\\gal-dl\\{folderName}";
+            Console.WriteLine(path);
             foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//a[@class='rel-link']"))
             {
                 string img = node.GetAttributeValue("href", null).ToString();
-                await img.DownloadFileAsync(@"C:\\Users\\Amir\\Reddit");
-                Console.WriteLine(img);
+                Console.WriteLine($"Downloading => {img}");
+                await img.DownloadFileAsync(path);
             }
+            Console.WriteLine("Finished Downlaoding!");
         }
+
+        public static async void MultiDownloadAsync()
+        {
+            Console.Clear();
+            Console.WriteLine("[+] Multi Download Mode");
+            Console.Write("Type in URL seperated by '+' => ");
+            string URLs = Console.ReadLine();
+            List<string> url_list = new List<string>();
+            foreach (string url in URLs.Split("+"))
+            {
+                url_list.Add(url.Remove(0, 35).Replace("/", ""));
+            }
+            
+            // HtmlWeb web = new HtmlWeb();
+            // var htmlDoc = web.Load(URL);
+
+            // foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//a[@class='rel-link']"))
+            // {
+            //     string img = node.GetAttributeValue("href", null).ToString();
+            //     Console.WriteLine($"Downloading => {img}");
+            //     await img.DownloadFileAsync(@"C:\\Users\\Amir\\gal-dl");
+            // }
+            // Console.WriteLine("Finished Downlaoding!");
+        } 
     }
 }
