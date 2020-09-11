ed78232f38d81731b3ee9d265fe3e78de9b1057f
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2020-09-11 09:18:32 +0430

    its done, but need working

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
-using Flurl;
-
+using System.Threading.Tasks;
+using Flurl.Http;
+using HtmlAgilityPack;
 namespace gal_dl
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Title = "Gallery Downloader";
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+
+            string URL = "https://www.pornpics.com/galleries/puba-network-anna-bell-peaks-jenevieve-hexxx-74787898/";
+            HtmlWeb web = new HtmlWeb();
+            FlurlRequest f = new FlurlRequest(URL);
+            var htmlDoc = web.Load(URL);
+
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//a[@class='rel-link']"))
+            {
+                string img = node.GetAttributeValue("href", null).ToString();
+                await img.DownloadFileAsync(@"C:\\Users\\Amir\\Reddit");
+                Console.WriteLine(img);
+            }
         }
     }
 }
