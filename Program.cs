bf565d7e9853d73dcb9e807ead95283710befe28
Amir Hossein Roohi <ahroohi1379@hotmail.com>
2020-09-11 08:31:07 +0430

    generaoted project plus added package

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Flurl;
+
+namespace gal_dl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
